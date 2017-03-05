19af8c2308dcd41137249c41b65f94f93b9086fa
unknown <Tyler_Fiacco@yahoo.com>
2017-03-05 06:51:28 -0500

    group manager

---
+++ server/GroupManager.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class GroupManager {
+  
+  // Feilds
+  ArrayList<User> users = new ArrayList<User>();
+  
+  
+  // Constructors
+  public GroupManger() {
+    users = null;
+  }
+  
+  
+  // Functions
+  
+}
