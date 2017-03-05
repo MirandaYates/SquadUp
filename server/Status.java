0c76413813ba9086fb88dfd8d27485cff97653a9
MirandaYates <MirandaYates12@gmail.com>
2017-03-05 07:47:51 -0500

    status in progress

--- server/Status.java
+++ server/Status.java
@@ -1,28 +1,37 @@
 import java.util.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 //Status object has getters and setters
-public class Status{
+public class Status extends DAO{
     private int status_code; //use -1, 0, 1 for bad, neutral, good
-    private String time_of_last_checkin;
+    private String status_time;
     
-    public Status(String time){
-        this.status_code= 1;    //just made the group
-        this.time_of_last_checkin= //
-    }
-    
-    public int getStatusCode(){
+    public int getStatusCode(int u){
         return this.status_code;
     }
     
-    public String getTimeOfLastCheckin(){
+    public String getStatusTime(int u){
         return this.time_of_last_checkin;
     }
     
-    public void setStatusCode(int x){
+    public void setStatusCode(int u, int x){
         this.status_code= x;
+        String sql = "insert into status (user_id, status, datetime) values (?, ?, now())"
+        try{
+            Connection connection = super.getConnection();
+            PreparedStatement statement = connection.prepareStatement(sql);
+            statement.setString(1, u);
+            statement.setString(2, x);
+            this.id = executeQuery(connection, statement).get(0).get(0);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
     }
     
-    public void setTimeOfLastcheckin(String s){
+    public void setStatusTime(int u){
         this.time_of_last_checkin= s;
     }
 }
