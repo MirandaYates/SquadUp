5c4de54eb23f182696f4c9436b216aa7fe0ca4fa
MirandaYates <MirandaYates12@gmail.com>
2017-03-05 07:41:11 -0500

    User for short time usage

--- server/User.java
+++ server/User.java
@@ -1,15 +1,15 @@
 //User Object
 //accessor methods for: name, id, status, gps
 //mutator method for: status, gps
-import java.sql.connection;
+import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class User extends DAO{
 	private int id;
 	private String name;
-	private Status status = new Status();
-	private String gps = null;
+	private Status status;
+	private String gps;
 
 	public User(String username){
 		this.name = username;
@@ -18,7 +18,7 @@ public class User extends DAO{
 			Connection connection = super.getConnection();
 			PreparedStatement statement = connection.prepareStatement(sql);
 			statement.setString(1, username);
-			this.id = executeQuery(connection, statement).get(0).get(0);
+			this.id = executeQuery(connection, statement).getRow(0).get(0);
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
@@ -26,11 +26,13 @@ public class User extends DAO{
 
 	public int getID(){ return id; }
 	public String getName(){ return name; }
-	public int getStatus(){	return status.getStatusCode(); }
-	public String getGPS(){	return gps.getGPSCode(); }
+	public int getStatus(){	return status.getStatusCode(id); }
+	public int getStatusTime(){	return status.getStatusTime(id); }
+	public String getGPS(){	return gps.getGPSCode(id); }
+	public String getGPSTime(){	return gps.getGPSTime(id); }
 
 	//sets status to 's'
-	public setStatus(s){ status.setStatusCode(s) }
+	public setStatus(s){ status.setStatusCode(id, s) }
 	//sets gps to 's'
-	public setGPS(s){ status.setGPS(s) }
+	public setGPS(s){ status.setGPS(id, s) }
 }
